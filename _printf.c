f27e985307662635fa3ec9fe2ccee7bc2b860df3
curtarnolds <afayedormawuli@gmail.com>
2023-08-07 11:01:56 +0000

    Handled 'cs%'

---
+++ _printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+
+/**
+ * _printf - Mimics the standard library printf
+ * @format: A format specifier
+ * Return: The number of characters printed, excluding the null byte
+ * at the end of the string
+*/
+int _printf(const char *format, ...)
+{
+	char *temp;
+	int i = 0, j = 0;
+	va_list arg_list;
+	va_start(arg_list, format);
+
+	while (format[i])
+	{
+		if (format[i] == '\\' && format[i + 1] != ' ')
+		{
+			switch (format[i + 1])
+			{
+				case 'n':
+					_putchar('\n');
+					break;
+
+				case 't':
+					_putchar('\t');
+					break;
+
+				default:
+					_putchar('\\');
+					break;
+			}
+			i++;
+		}
+		else if (format[i] == '%')
+		{
+			while (format[i + 1] == ' ')
+				i++;
+			switch (format[i + 1])
+			{
+				case 'c':
+					_putchar(va_arg(arg_list, int));
+					break;
+				case 's':
+					temp = va_arg(arg_list, char *);
+					write(1, temp, strlen(temp));
+					break;
+				case '%':
+					_putchar('%');
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i + 1]);
+					break;
+			}
+			i++;
+		}
+		else
+			_putchar(format[i]);
+		i++;
+	}
+	va_end(arg_list);
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFFER 1024
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
